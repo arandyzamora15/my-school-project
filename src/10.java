cae1e4c8cbdd360cf3dc69b9e63dc6e59abab397
arandyzamora15 <arandyzamora15@gmail.com>
2025-03-15 21:46:47 +0000

    Add 10.java

---
+++ src/10.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+public class my-school-project{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x = 5;
+        if (x == 4) {
+            System.out.println("The value of x is 4");
+        } else {
+            System.out.println("The value of x is not 4");
+        }
+    }
+}
