b4a9c969dae314b14682c972c3249ac5e4d9d7e4
arandyzamora15 <arandyzamora15@gmail.com>
2025-03-20 12:26:22 +0000

    Add 15.java

---
+++ src/15.java
@@ -0,0 +1,9 @@
+public class SchoolProject {
+    public static void main(String[] args) {
+        // Example code to demonstrate various concepts like object-oriented programming, basic input-output operations, and data structures
+        System.out.println("Hello, World!");
+        int age = 18;
+        String name = "John";
+        double height = 1.70;
+    }
+}
