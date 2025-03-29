9fa46a2ad444a2d02e65409b9dca092016736e3f
arandyzamora15 <arandyzamora15@gmail.com>
2025-03-29 19:46:38 +0000

    Add 20.java

---
+++ src/20.java
@@ -0,0 +1,13 @@
+public class SchoolProject {
+    public static void main(String[] args) {
+        // Example code to demonstrate Java syntax
+        System.out.println("Hello, World!");
+        
+        int age = 18;
+        if (age >= 18) {
+            System.out.println("You are old enough.");
+        } else {
+            System.out.println("You are not old enough.");
+        }
+    }
+}
